copr-ebbex/mingw-gettext

commit 74b5a7644ba8e184a391cec05e7e159b1e3d2410

Fix SONAME of libgettext* DLLs
These were not found during the mass rebuild:

    File not found: /home/rjones/rpmbuild/BUILDROOT/mingw-gettext-0.22-2.fc39.x86_64/usr/i686-w64-mingw32/sys-root/mingw/bin/libgettextlib-0-21-1.dll
    File not found: /home/rjones/rpmbuild/BUILDROOT/mingw-gettext-0.22-2.fc39.x86_64/usr/i686-w64-mingw32/sys-root/mingw/bin/libgettextsrc-0-21-1.dll

Local examination shows that the version number has changed to 0-22.

After fixing that, we get this error:

error: Installed (but unpackaged) file(s) found:
   /usr/i686-w64-mingw32/sys-root/mingw/lib/libgettextlib.a
   /usr/x86_64-w64-mingw32/sys-root/mingw/lib/libgettextlib.a

mingw-gettext.spec
@@ -122,9 +122,9 @@ find %{buildroot} -name "*.la" -delete
 %{mingw32_bindir}/gettext.sh
 %{mingw32_bindir}/gettextize
 %{mingw32_bindir}/libasprintf-0.dll
-%{mingw32_bindir}/libgettextlib-0-21-1.dll
+%{mingw32_bindir}/libgettextlib-0-22.dll
 %{mingw32_bindir}/libgettextpo-0.dll
-%{mingw32_bindir}/libgettextsrc-0-21-1.dll
+%{mingw32_bindir}/libgettextsrc-0-22.dll
 %{mingw32_bindir}/libintl-8.dll
 %{mingw32_bindir}/libtextstyle-0.dll
 %{mingw32_bindir}/msg*.exe
@@ -140,6 +140,7 @@ find %{buildroot} -name "*.la" -delete
 %{mingw32_includedir}/textstyle/woe32dll.h
 %{mingw32_libdir}/libasprintf.dll.a
 %{mingw32_libdir}/libgettextlib.dll.a
+%{mingw32_libdir}/libgettextlib.a
 %{mingw32_libdir}/libgettextpo.dll.a
 %{mingw32_libdir}/libgettextsrc.dll.a
 %{mingw32_libdir}/libintl.dll.a
@@ -163,9 +164,9 @@ find %{buildroot} -name "*.la" -delete
 %{mingw64_bindir}/gettext.sh
 %{mingw64_bindir}/gettextize
 %{mingw64_bindir}/libasprintf-0.dll
-%{mingw64_bindir}/libgettextlib-0-21-1.dll
+%{mingw64_bindir}/libgettextlib-0-22.dll
 %{mingw64_bindir}/libgettextpo-0.dll
-%{mingw64_bindir}/libgettextsrc-0-21-1.dll
+%{mingw64_bindir}/libgettextsrc-0-22.dll
 %{mingw64_bindir}/libintl-8.dll
 %{mingw64_bindir}/libtextstyle-0.dll
 %{mingw64_bindir}/msg*.exe
@@ -181,6 +182,7 @@ find %{buildroot} -name "*.la" -delete
 %{mingw64_includedir}/textstyle/woe32dll.h
 %{mingw64_libdir}/libasprintf.dll.a
 %{mingw64_libdir}/libgettextlib.dll.a
+%{mingw64_libdir}/libgettextlib.a
 %{mingw64_libdir}/libgettextpo.dll.a
 %{mingw64_libdir}/libgettextsrc.dll.a
 %{mingw64_libdir}/libintl.dll.a

commit 415095340aef7dad3a66a462e194250906164f42

Remove libgettextlib.a
When building under Koji:

error: File not found: /builddir/build/BUILDROOT/mingw-gettext-0.22-2.fc39.noarch/usr/i686-w64-mingw32/sys-root/mingw/lib/libgettextlib.a

Somehow Koji runs extra rules that delete the *.a files, unclear why.

Fixes: commit 74b5a7644ba8e184a391cec05e7e159b1e3d2410

mingw-gettext.spec
@@ -140,7 +140,6 @@ find %{buildroot} -name "*.la" -delete
 %{mingw32_includedir}/textstyle/woe32dll.h
 %{mingw32_libdir}/libasprintf.dll.a
 %{mingw32_libdir}/libgettextlib.dll.a
-%{mingw32_libdir}/libgettextlib.a
 %{mingw32_libdir}/libgettextpo.dll.a
 %{mingw32_libdir}/libgettextsrc.dll.a
 %{mingw32_libdir}/libintl.dll.a
@@ -182,7 +181,6 @@ find %{buildroot} -name "*.la" -delete
 %{mingw64_includedir}/textstyle/woe32dll.h
 %{mingw64_libdir}/libasprintf.dll.a
 %{mingw64_libdir}/libgettextlib.dll.a
-%{mingw64_libdir}/libgettextlib.a
 %{mingw64_libdir}/libgettextpo.dll.a
 %{mingw64_libdir}/libgettextsrc.dll.a
 %{mingw64_libdir}/libintl.dll.a